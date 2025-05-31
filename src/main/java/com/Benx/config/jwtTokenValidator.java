8084382c474909eef7c8904751f6d76aae9702d3
zinnith vmj <zinnithvmj@gmail.com>
2025-05-31 16:45:33 +0530

    Initial commit

---
+++ src/main/java/com/Benx/config/jwtTokenValidator.java
@@ -0,0 +1,20 @@
+package com.Benx.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+public class jwtTokenValidator extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+
+    }
+}
